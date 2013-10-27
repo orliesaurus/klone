4d0833e6cecd68dfed1e1a80630a5e4eade3fe36
Marcello Seri <marcello.seri@gmail.com>
2013-10-27 11:34:46 +0000

    Sticazzi

--- js/cosanostra.js
+++ js/cosanostra.js
@@ -8,20 +8,25 @@ jQuery.fn.extend({
 function presentaImmagine(picUrl, compareUrl) {
 
   $('#sketch').css("background-color","#222");
+  $('#imgappr').fadeIn(1);
   $('#imgappr').html('<img src=\"'+ picUrl +'\" height=\"'+dimens.toString()+'px\"/>');
   $('#imgappr').fadeOut(4000, function(){ $('#sketch').css('background-color','black') });
 
-  $('.yolocount').countdown({
-        date: +(new Date) + 16000,
-        render: function(data) {
-      if (data.sec >= 1) {
-        $(this.el).text(this.leadingZeros(data.sec-1, 2) + " sec")
-      }
-      },
-      onEnd: function() {
-        confrontaCon(compareUrl);
-        //reset the screen and go to next level
-      }
-  });
+  if($('.yolocount').data('countdown') !== null && $('.yolocount').data('countdown') !== undefined ) {
+    $('.yolocount').data('countdown').update(+(new Date) + 16000);
+  } else {
+    $('.yolocount').countdown({
+          date: +(new Date) + 16000,
+          render: function(data) {
+        if (data.sec >= 1) {
+          $(this.el).text(this.leadingZeros(data.sec-1, 2) + " sec")
+        }
+        },
+        onEnd: function() {
+          confrontaCon(compareUrl);
+          //reset the screen and go to next level
+        }
+    });
+  }  
 
 }
